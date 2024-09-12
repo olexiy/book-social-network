f13dad74cb8befccd54660ac807944fc88fd782f
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2024-09-12 11:26:57 +0200

    testing with testcontainers optimization with @ServiceConnection

--- book-network/src/test/java/de/olexiy/demo/book_network/BookNetworkApiApplicationTests.java
+++ book-network/src/test/java/de/olexiy/demo/book_network/BookNetworkApiApplicationTests.java
@@ -2,8 +2,7 @@ package de.olexiy.demo.book_network;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -12,18 +11,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 class BookNetworkApiApplicationTests {
 	@Container
-	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13.3")
-			.withDatabaseName("testdb")
-			.withUsername("test")
-			.withPassword("test");
-
-	@DynamicPropertySource
-	static void registerPgProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-	}
-
+	@ServiceConnection
+	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres");
 	@Test
 	void contextLoads() {
 	}
